a8e0cb1563d3fe377958d9935a8e8c40331b8688
Todd D <todd.danos@doe.mass.edu>
2020-02-04 12:49:11 -0500

    Updated GetUsers-AllSiteCollections.ps1 with logging
    Updated the gitHub version to the latest HD version.

--- GetUsers-AllSiteCollections.ps1
+++ GetUsers-AllSiteCollections.ps1
@@ -7,34 +7,60 @@
 
 ##################################################################
 
+# Add LogWrite calls along with Write-host calls
+$fdate = (get-date).ToString("yyMM")
+$Logfile = "C:\temp\SharePoint Online\Users-$(gc env:computername)-$fdate.log"
+
+Function LogWrite
+{
+    Param ([string]$logstring)
+    Add-content $Logfile -value $logstring
+}
+
 $host.Runspace.ThreadOptions = "ReuseThread"
 
 #Definition of the function that gets all the site collections information in a SharePoint Online tenant
 function Get-SPOUsersAllSiteCollections
 {
-param ($sUserName,$sMessage)
-try
-{ 
- Write-Host "----------------------------------------------------------------------------" -foregroundcolor Green
-Write-Host "Getting the information for all the site colletions in the Office 365 tenant" -foregroundcolor Green
-Write-Host "----------------------------------------------------------------------------" -foregroundcolor Green
-$msolcred = get-credential -UserName $sUserName -Message $sMessage
-Connect-SPOService -Url $sSPOAdminCenterUrl -Credential $msolcred
-$spoSites=Get-SPOSite | Select *
-foreach($spoSite in $spoSites)
-{
-Write-Host "Users for " $spoSite.Url -foregroundcolor Blue
-Get-SPOUser -Site $spoSite.Url
-Write-Host
-} 
- Write-Host "Getting users" -ForegroundColor Green 
-Get-SPOUser -Site "https://masseoe.sharepoint.com" | Out-File "C:\temp\SharePoint Online\Users.txt" -Append;
+    param ($sUserName,$sMessage)
+    try
+    { 
+        Write-Host "----------------------------------------------------------------------------" -foregroundcolor Green
+        Write-Host "Getting the information for all the site colletions in the Office 365 tenant" -foregroundcolor Green
+        Write-Host "----------------------------------------------------------------------------" -foregroundcolor Green
+        #   LogWrite "----------------------------------------------------------------------------"
+        #   LogWrite "Getting the information for all the site colletions in the Office 365 tenant"
+        #   LogWrite "----------------------------------------------------------------------------"
+        echo (get-date).ToString("yyyyMMdd") | Out-File $Logfile -Append;
+        echo "----------------------------------------------------------------------------" | Out-File $Logfile -Append;
+        echo "Getting the information for all the site colletions in the Office 365 tenant" | Out-File $Logfile -Append;
+        echo "----------------------------------------------------------------------------" | Out-File $Logfile -Append;
 
- }
-catch [System.Exception]
-{
-write-host -f red $_.Exception.ToString() 
- } 
+        $msolcred = get-credential -UserName $sUserName -Message $sMessage
+        Connect-SPOService -Url $sSPOAdminCenterUrl -Credential $msolcred
+        $spoSites=Get-SPOSite | Select *
+
+        foreach($spoSite in $spoSites)
+        {
+            Write-Host "Users for " $spoSite.Url -foregroundcolor Blue
+            $Url = $spoSite.Url
+         #   LogWrite "Users for $Url`r`n"
+            echo "Users for $Url" | Out-File $Logfile -Append;
+         #   Get-SPOUser -Site $spoSite.Url
+            Get-SPOUser -Site $spoSite.Url | Out-File $Logfile -Append;
+         #   $SPOLUser = Get-SPOUser -Site $spoSite.Url
+            Write-Host
+         #   LogWrite "`r`n"
+            echo "`r`n" | Out-File $Logfile -Append;
+        } 
+     #   Write-Host "Getting users" -ForegroundColor Green
+     #   LogWrite "`r`nGetting users"
+     #   Get-SPOUser -Site "https://masseoe.sharepoint.com" | Out-File $Logfile -Append;
+    }
+    catch [System.Exception]
+    {
+        write-host -f red $_.Exception.ToString() 
+    } 
 }
 
 #Connection to Office 365
